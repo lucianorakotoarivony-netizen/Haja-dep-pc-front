ef803d62f9433ad2337745dbe3d7a83cc170e527
KheyroW <luciano.rakotoarivony@gmail.com>
2026-04-28 03:48:15 +0300

    fix url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
+    production:false,
     apiUrl: "http://localhost:3000/api",
     baseUrl:"http://localhost:3000",
     sitekey: '10000000-ffff-ffff-ffff-000000000001',
